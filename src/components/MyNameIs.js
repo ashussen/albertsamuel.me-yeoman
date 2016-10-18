775962e3ea9c586f334c237da0b546802def15df
Albert Samuel <albert@albertubuntu.Home>
2016-10-18 18:39:59 +1100

    Designed header section layout

--- src/components/MyNameIs.js
+++ src/components/MyNameIs.js
@@ -1,20 +1,31 @@
+/*global jQuery:true*/
 var React = require('react');
-
 var MyNameIs = React.createClass({
+  componentDidMount: function() {
+    jQuery('.wide.image').animate({padding:'200px 0 200px 0'});
+  },
   render: function(){
     var wideImage = {
       backgroundImage: 'url(../images/Background.jpg)',
       backgroundSize: 'cover',
       backgroundPosition: '49% 50%',
-      backgroundRepeat: 'no-repeat no-repeat'
+      backgroundRepeat: 'no-repeat no-repeat',
+      paddingTop: 10,
+      paddingBottom: 10
+    }
+    var container = {
+      paddingTop: 90,
+      paddingBottom: 70,
+      position: 'relative',
+      margin: '0 0 0 5%',
+      width: 'auto'
     }
     return (
-      <li className="slide selected" tabIndex="0">
+      <li className="slide selected my-name-is" tabIndex="0">
         <a className="section-anchor" data-scroll-name="#1"></a>
         <a className="section-name-anchor" data-section-name="Me"></a>
         <div className="wide image resize strikingly-light-text lazy-background hasImage" style={wideImage}>
-          <div className="video-bg-wrap" data-video-html="" style={{display: 'none'}}></div>
-          <div className="container">
+          <div className="container" style={container}>
             <div className="sixteen columns">
               <div className="spacer" style={{height:10}}>&nbsp;</div>
               <h3 className="subtitle">
@@ -22,18 +33,18 @@ var MyNameIs = React.createClass({
                 </span>
                 <div className="ib">
                   <div data-component="text" data-name="text1" data-text-type="body" className="text-component">
-                    <div className="content content2" style={{fontFamily: 'open sans'}}>
-                      <div>Hey, I'm</div>
+                    <div className="content content2" style={{fontFamily: 'droid serif', color: '#ebeced'}}>
+                      <div>Hi, I am </div>
                     </div>
                   </div>
                 </div>
                 <span className="line">
                 </span>
               </h3>
-              <h1 className="title big">
+              <h1 className="title" style={{fontSize: '450%'}}>
                 <div data-component="text" data-name="text2" data-text-type="title" className="text-component">
-                  <div className="content content2" style={{fontFamily: 'bebas neue'}}>
-                    <div>Gary Sheng.</div>
+                  <div className="content content2" style={{fontFamily: 'ubuntu', color: '#ebeced'}}>
+                    <div>Albert Samuel</div>
                     <div>&nbsp;</div>
                   </div>
                 </div>
